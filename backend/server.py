47f3cfc1db9a98bf82c3b782274a60affc6793ee
benpankow <pankow.ben@gmail.com>
2020-04-06 00:58:43 -0500

    Add basic client/server interaction

--- backend/server.py
+++ backend/server.py
@@ -3,20 +3,21 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 game_state = {
-        'dial_position' : 50
+        'dial_position' : 0.5
         }
 
 @socketio.on('requestGameState')
 def handle_request_game_state():
-    emit('gameState', game_state, json=True, broadcast=True)
+    socketio.emit('gameState', game_state, json=True, broadcast=True)
 
 @socketio.on('setDialPosition')
 def handle_set_dial_position(j):
+    print(j)
     game_state['dial_position'] = j['dial_position']
-    emit('gameState', game_state, json=True, broadcast=True)
+    socketio.emit('gameState', game_state, json=True, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 import "./App.scss";
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
@@ -11,14 +12,33 @@ class App extends React.Component {
   }
 }
 
-class Device extends React.Component {
+class Device extends Component {
   constructor(props) {
     super(props);
 
     this.deviceInner = React.createRef();
     this.state = {
-      rotation: 0,
+      gameState: {
+        dialPosition: 0.5,
+      },
+      client: null,
     };
+  }
+
+  componentDidMount() {
+    const client = socketIOClient("http://127.0.0.1:5000/");
+
+    client.on("connect", () => {
+      client.emit("requestGameState");
+    });
+    client.on("gameState", (updatedState) => {
+      this.setState({
+        gameState: updatedState,
+      });
+    });
+    this.setState({
+      client: client,
+    });
   }
 
   dialClicked = (event) => {
@@ -30,13 +50,27 @@ class Device extends React.Component {
     const xOffset = event.clientX - xOrigin;
     const yOffset = event.clientY - yOrigin;
 
+    let rotation = 2 * Math.PI - Math.atan2(xOffset, yOffset);
+    if (rotation > 2 * Math.PI) {
+      rotation -= 2 * Math.PI;
+    }
+
+    let dialPosition = rotation / Math.PI - 0.5;
+    dialPosition = Math.min(dialPosition, 0.95);
+    dialPosition = Math.max(dialPosition, 0.05);
+
     this.setState({
-      rotation: Math.PI - Math.atan2(xOffset, yOffset),
+      gameState: {
+        dialPosition: dialPosition,
+      },
     });
+
+    this.state.client.emit("setDialPosition", this.state.gameState);
   };
 
   render() {
-    const { rotation } = this.state;
+    const { dialPosition } = this.state.gameState;
+    const rotation = Math.PI * (dialPosition + 1.5);
 
     return (
       <div className="device_parent">
@@ -55,14 +89,14 @@ class Device extends React.Component {
                 className="dial"
                 style={{ transform: `rotate(${rotation}rad)` }}
               />
-              <div
-                className="target"
-                style={{ transform: `rotate(${rotation}rad)` }}
-              >
-                <span class="number">4</span>
+              <div className="target" style={{ transform: `rotate(26.8deg)` }}>
+                <span className="number">3</span>
               </div>
-              <div className="target" style={{ transform: `rotate(13deg)` }}>
-                <span class="number">3</span>
+              <div className="target" style={{ transform: `rotate(20deg)` }}>
+                <span className="number">4</span>
+              </div>
+              <div className="target" style={{ transform: `rotate(13.2deg)` }}>
+                <span className="number">3</span>
               </div>
             </div>
           </div>
